6a2f704f9f3df79abb7196be97b23668a5ed31d2
joseph03 <joseph03sg@gmail.com>
2025-04-02 20:56:16 +0800

    2nd run

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 terraform {
   backend "s3" {
-    bucket = "joseph-sctps3bucket"  # "sctp-ce9-tfstate"
-    key    = "joseph03-wk18-3.1"  
+    bucket = "joseph-sctps3bucket" # "sctp-ce9-tfstate"
+    key    = "joseph03-wk18-3.1"
     region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+            region = "us-east-1"
 }
 
 
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {

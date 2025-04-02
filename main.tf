d8f3df30a2f512ca90b970278a0cd05780cbd15f
joseph03 <joseph03sg@gmail.com>
2025-04-02 21:09:37 +0800

    4th

--- main.tf
+++ main.tf
@@ -17,6 +17,9 @@ locals {
   name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
+terraform {
+  required_version = ">= 1.0"
+}
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
